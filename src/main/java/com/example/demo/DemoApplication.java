e40fd768342c4494c1f4eb655f8122184faccb6a
lalkumar-dev <javainfo.lk@gmail.com>
2025-08-28 01:31:01 +0530

    defect-auth-2345-signin

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		int x=200;
+		int y=500;
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
